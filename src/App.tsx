2b2d18e224e6c9347b51a5e8e2f40ef976f6518f
Kunwarpreet <singhkunwarpreet797@gmail.com>
2024-02-23 15:08:04 +0530

    added interface and enum in person file and edited app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import Person from './components/Person';
+import {Person, Country} from './components/Person';
 
 function App() {
   return (
     <div className="App">
-      <Person name={'kunwar'} email={'singhkunwarpreet797@gmail.com'} friends={['rahul','raj','mohan']}/>
+      <Person name={'kunwar'} 
+      email={'singhkunwarpreet797@gmail.com'} 
+      friends={['rahul','raj','mohan']}
+      country={Country.Brazil}/>
     </div>
   );
 }
--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -2,15 +2,23 @@ interface Prop{
     name:string;
     email:string;
     friends:string[];
+    country:Country;
 }
-const Person = (props:Prop) => {
+
+export enum Country{
+  Brazil='Brazil',
+  USA='USA',
+  France='France'
+}
+
+export const Person = (props:Prop) => {
   return (
     <div>
         <h1>{props.name}</h1>
         <h1>{props.email}</h1>
         <div>{props.friends.map((friend: string)=><h1>{friend}</h1>)}</div>
+        <h1>{props.country}</h1>
     </div>
   )
 }
 
-export default Person
